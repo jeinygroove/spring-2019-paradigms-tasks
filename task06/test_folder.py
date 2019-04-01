0eddd91903720f28db8b7d3f5221d9f13710b80e
Ольга Шиманская <st069066@student.spbu.ru>
2019-04-02 00:38:47 +0300

    add test_folder.py - tests for folder.py

---
+++ task06/test_folder.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+from folder import *
+from model import *
+
+
+def fold_constants(program):
+    const_folder = ConstantFolder()
+    return program.accept(const_folder)
+
+
+def test_end_to_end():
+    program = BinaryOperation(
+        Number(10),
+        '-',
+        UnaryOperation(
+            '-',
+            BinaryOperation(
+                Number(3),
+                '+',
+                BinaryOperation(
+                    Reference('x'),
+                    '-',
+                    Reference('x')
+                )
+            )
+        )
+     )
+    assert fold_constants(program) == Number(13)
+
+
+def test_num_op_num():
+    program = BinaryOperation(
+        Number(10),
+        '+',
+        Number(32)
+    )
+    assert fold_constants(program) == Number(42)
+
+
+def test_num0_multiply_ref():
+    program = BinaryOperation(
+        Number(0),
+        '*',
+        Reference('x')
+    )
+    assert fold_constants(program) == Number(0)
+
+
+def test_ref_multiply_num0():
+    program = BinaryOperation(
+        Reference('x'),
+        '*',
+        Number(0)
+    )
+    assert fold_constants(program) == Number(0)
+
+
+def test_subtract_equal_refs():
+    program = BinaryOperation(
+       Reference('x'),
+       '-',
+       Reference('x')
+    )
+    assert fold_constants(program) == Number(0)
+
+
+def test_negative_num():
+    program = UnaryOperation(
+        '-',
+        Number(73)
+    )
+    assert fold_constants(program) == Number(-73)
