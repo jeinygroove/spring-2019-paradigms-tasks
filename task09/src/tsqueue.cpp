cca962b2dafab9a4282bb57459e41a6233625f25
Ольга Шиманская <st069066@student.spbu.ru>
2019-04-27 18:41:35 +0300

    added fixes

--- task09/src/tsqueue.cpp
+++ task09/src/tsqueue.cpp
@@ -3,11 +3,11 @@
 void threadsafe_queue_init(ThreadsafeQueue *q) {
     queue_init(&q->q);
     pthread_mutex_init(&q->mutex, NULL);
-    pthread_cond_init(&q->cond, NULL);
+    pthread_cond_init(&q->cond_not_empty_queue, NULL);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
-    pthread_cond_destroy(&q->cond);
+    pthread_cond_destroy(&q->cond_not_empty_queue);
     pthread_mutex_destroy(&q->mutex);
     queue_destroy(&q->q);
 }
@@ -15,14 +15,14 @@ void threadsafe_queue_destroy(ThreadsafeQueue *q) {
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
     pthread_mutex_lock(&q->mutex);
     queue_push(&q->q, data);
-    pthread_cond_signal(&q->cond);
+    pthread_cond_signal(&q->cond_not_empty_queue);
     pthread_mutex_unlock(&q->mutex);
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
     pthread_mutex_lock(&q->mutex);
     while (queue_empty(&q->q)) {
-        pthread_cond_wait(&q->cond, &q->mutex);
+        pthread_cond_wait(&q->cond_not_empty_queue, &q->mutex);
     }
     void *result = queue_pop(&q->q);
     pthread_mutex_unlock(&q->mutex);
--- task09/src/tsqueue_test.cpp
+++ task09/src/tsqueue_test.cpp
@@ -45,10 +45,8 @@ TEST_CASE("ThreadsafeQueue multithreaded ping-pong") {
         for (int i = 0; i < PING_PONGS; i++) {
             int number = 73;
             threadsafe_queue_push(&qs[0], &number);
-            int *result =
-                static_cast<int *>(threadsafe_queue_wait_and_pop(&qs[1]));
             REQUIRE(number == 74);
-            REQUIRE(result == &number);
+            REQUIRE(static_cast<int *>(threadsafe_queue_wait_and_pop(&qs[1])) == &number);
         }
         return nullptr;
     };
@@ -67,8 +65,8 @@ TEST_CASE("ThreadsafeQueue multithreaded ping-pong") {
     pthread_t t1, t2;
     REQUIRE(pthread_create(&t1, nullptr, pinger, qs) == 0);
     REQUIRE(pthread_create(&t2, nullptr, ponger, qs) == 0);
-    REQUIRE(pthread_join(t1, nullptr) == 0);
     REQUIRE(pthread_join(t2, nullptr) == 0);
+    REQUIRE(pthread_join(t1, nullptr) == 0);
 
     threadsafe_queue_destroy(&qs[1]);
     threadsafe_queue_destroy(&qs[0]);
